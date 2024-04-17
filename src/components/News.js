9d4094d954357e52fa30078e1e24a6df6156ea98
ajay-wq <singhajaypratap839@gmail.com>
2024-04-17 10:14:30 +0530

    main: Added the Top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -32,15 +32,19 @@ export class News extends Component {
     }
 
     async updateNews() {
+      this.props.setProgress(0);
       const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5bb951e9fb08414d99a33970651ab076&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       this.setState({ loading: true });
       let data = await fetch(url);
+      this.props.setProgress(30);
       let parsedData = await data.json();
+      this.props.setProgress(70);
       this.setState({
         articles: parsedData.articles,
         totalResults: parsedData.totalResults,
         loading: false
       })
+      this.props.setProgress(100);
     }
     async componentDidMount() {
       this.updateNews();
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,10 +10,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1',left:'85%' }}>
-          {source}
-        </span>
-
+          <div>
+            <span className="badge rounded-pill bg-danger" style={{ display: 'flex', position: "absolute", right:'0'}} >
+              {source}
+            </span>
+          </div>
           <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2023/04/Collage-Maker-14-Apr-2023-12-26-PM-3953-1-770x433.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
