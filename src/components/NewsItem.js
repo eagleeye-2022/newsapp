91cd3a42eed405be8faa025f778ebf1bf24a474d
ajay-wq <singhajaypratap839@gmail.com>
2024-04-22 16:16:22 +0530

    main: Changes class based to function based and header sticky

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,33 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
+const NewsItem = (props)=> {
+  let { title, description, imageUrl, newsUrl, author, date, source} = props;
 
-
-
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source} = this.props;
-
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div>
-            <span className="badge rounded-pill bg-danger" style={{ display: 'flex', position: "absolute", right:'0'}} >
-              {source}
-            </span>
-          </div>
-          <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2023/04/Collage-Maker-14-Apr-2023-12-26-PM-3953-1-770x433.jpg":imageUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text"><small className="text-danger">By {author} on {new Date(date).toGMTString()}</small></p>
-            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">
-              Read More
-            </a>
-          </div>
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div>
+          <span className="badge rounded-pill bg-danger" style={{ display: 'flex', position: "absolute", right:'0'}} >
+            {source}
+          </span>
+        </div>
+        <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2023/04/Collage-Maker-14-Apr-2023-12-26-PM-3953-1-770x433.jpg":imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text"><small className="text-danger">By {author} on {new Date(date).toGMTString()}</small></p>
+          <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default NewsItem;
